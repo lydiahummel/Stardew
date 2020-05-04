fb2e306053b68e2deb617576677cf9bd0496d5fc
lydiahummel <61434444+lydiahummel@users.noreply.github.com>
2020-05-04 19:34:55 -0400

    html added, jquery and javascript added
    unnecessary photos also deleted

---
+++ StardewValley/js/app.js
@@ -0,0 +1,87 @@
+// disappearing and appearing image captions
+
+$(".captions").hide();
+
+$("#leah").hover(function() {
+    $("#leahcaption").fadeIn(2000);
+},
+                 function() {
+    $("#leahcaption").hide();
+});
+
+$("#sebastian").hover(function() {
+    $("#sebastiancaption").fadeIn(2000);
+},
+                 function() {
+    $("#sebastiancaption").hide();
+});
+
+$("#penny").hover(function() {
+    $("#pennycaption").fadeIn(2000);
+},
+                 function() {
+    $("#pennycaption").hide();
+});
+
+$("#elliott").hover(function() {
+    $("#elliottcaption").fadeIn(2000);
+},
+                 function() {
+    $("#elliottcaption").hide();
+});
+
+$("#haley").hover(function() {
+    $("#haleycaption").fadeIn(2000);
+},
+                 function() {
+    $("#haleycaption").hide();
+});
+
+$("#alex").hover(function() {
+    $("#alexcaption").fadeIn(2000);
+},
+                 function() {
+    $("#alexcaption").hide();
+});
+
+$("#abigail").hover(function() {
+    $("#abigailcaption").fadeIn(2000);
+},
+                 function() {
+    $("#abigailcaption").hide();
+});
+
+$("#harvey").hover(function() {
+    $("#harveycaption").fadeIn(2000);
+},
+                 function() {
+    $("#harveycaption").hide();
+});
+
+$("#emily").hover(function() {
+    $("#emilycaption").fadeIn(2000);
+},
+                 function() {
+    $("#emilycaption").hide();
+});
+
+$("#sam").hover(function() {
+    $("#samcaption").fadeIn(2000);
+},
+                 function() {
+    $("#samcaption").hide();
+});
+
+$("#maru").hover(function() {
+    $("#marucaption").fadeIn(2000);
+},
+                 function() {
+    $("#marucaption").hide();
+});
+
+$("#shane").hover(function() {
+    $("#shanecaption").fadeIn(2000);
+},
+                 function() {
+    $("#shanecaption").hide();
+});
